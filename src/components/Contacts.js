496c5d3ce6670259528b1c34dbbd737433254b06
juliancameron79 <juliancameron79@gmail.com>
2021-07-05 02:10:12 +0200

    Fine tuned design and layout

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -40,108 +40,122 @@ const Contacts = () => {
   };
 
   return (
-    <div id="contacts" className="contacts py-5">
-      <div className="text-center">
-        <h1 className="contacts-heading my-5" data-aos="fade-right" data-aos-duration="1000">
-          contact me
-        </h1>
-        <p>Please fill out the form and I'll contact you as soon as possible.</p>
-        <span className="success-message">{successMessage}</span>
-      </div>
+    <section className="site-section bg-light" id="contacts">
       <div className="container">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="row">
-            <div className="col-md-1"></div>
-            <div className="col-md-5 col-sm-12">
-              {/* NAME INPUT */}
-              <div className="text-center">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Name"
-                  name="name"
-                  {...register("name", {
-                    required: "Please enter your name",
-                    maxLength: {
-                      value: 20,
-                      message: "Please enter a name with fewer than 20 characters",
-                    },
-                  })}
-                />
-                <div className="line"></div>
-              </div>
-              <span className="error-message">{errors.name && errors.name.message}</span>
-              {/* PHONE INPUT */}
-              <div className="text-center">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Phone Number"
-                  name="phone"
-                  {...register("phone", {
-                    required: "Please enter your phone number",
-                  })}
-                />
-                <div className="line"></div>
-              </div>
-              <span className="error-message">{errors.phone && errors.phone.message}</span>
-              {/* EMAIL INPUT */}
-              <div className="text-center">
-                <input
-                  type="email"
-                  className="form-control"
-                  placeholder="Email"
-                  name="email"
-                  {...register("email", {
-                    required: "Please provide your email",
-                    pattern: {
-                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                      message: "invalid email address",
-                    },
-                  })}
-                />
-                <div className="line"></div>
-              </div>
-              <span className="error-message">{errors.email && errors.email.message}</span>
-              {/* SUBJECT INPUT */}
-              <div className="text-center">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Subject"
-                  name="subject"
-                  {...register("subject", {
-                    required: "OOPS, you forget to add the subject.",
-                  })}
-                />
-                <div className="line"></div>
-              </div>
-              <span className="error-message">{errors.subject && errors.subject.message}</span>
-            </div>
-            <div className="col-md-5 col-sm-12">
-              {/* DESCRIPTION INPUT */}
-              <div className="text-center">
-                <textarea
-                  type="text"
-                  className="form-control"
-                  placeholder="Please leave your message..."
-                  name="description"
-                  {...register("description", {
-                    required: "Please describe your project needs or a message...",
-                  })}
-                ></textarea>
-                <div className="line"></div>
-              </div>
-              <span className="error-message">{errors.description && errors.description.message}</span>
-              <button className="btn-main-offer contact-btn" type="submit">
-                contact me
-              </button>
-            </div>
-            <div className="col-md-1"></div>
+        <div className="contacts">
+          <div className="text-center">
+            <h1 className="contacts-heading mb-5" data-aos="fade-right" data-aos-duration="1000">
+              <div className="line-contact me-3" data-aos="fade-right" data-aos-duration="1000"></div>
+              contact me
+              <div className="line-contact ms-3" data-aos="fade-right" data-aos-duration="1000"></div>
+            </h1>
+            <p>Please fill out the form and I'll contact you as soon as possible.</p>
+            <span className="success-message">{successMessage}</span>
           </div>
-        </form>
+          <div className="container">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="row">
+                <div className="col-md-12 form-group">
+                  <div className="row d-flex justify-content-center">
+                    <div className="col-md-6">
+                      {/* NAME INPUT */}
+                      <div className="text-center">
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Name"
+                          name="name"
+                          {...register("name", {
+                            required: "Please enter your name",
+                            maxLength: {
+                              value: 20,
+                              message: "Please enter a name with fewer than 20 characters",
+                            },
+                          })}
+                        />
+                        <div className="line"></div>
+                      </div>
+                      <span className="error-message">{errors.name && errors.name.message}</span>
+                      {/* PHONE INPUT */}
+                      <div className="text-center">
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Phone Number"
+                          name="phone"
+                          {...register("phone", {
+                            required: "Please enter your phone number",
+                          })}
+                        />
+                        <div className="line"></div>
+                      </div>
+                      <span className="error-message">{errors.phone && errors.phone.message}</span>
+                      {/* EMAIL INPUT */}
+                      <div className="text-center">
+                        <input
+                          type="email"
+                          className="form-control"
+                          placeholder="Email"
+                          name="email"
+                          {...register("email", {
+                            required: "Please provide your email",
+                            pattern: {
+                              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                              message: "invalid email address",
+                            },
+                          })}
+                        />
+                        <div className="line"></div>
+                      </div>
+                      <span className="error-message">{errors.email && errors.email.message}</span>
+                      {/* SUBJECT INPUT */}
+                      <div className="text-center">
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Subject"
+                          name="subject"
+                          {...register("subject", {
+                            required: "OOPS, you forget to add the subject.",
+                          })}
+                        />
+                        <div className="line"></div>
+                      </div>
+                      <span className="error-message">{errors.subject && errors.subject.message}</span>
+                    </div>
+                    {/* 111 */}
+                    <div className="col-md-6">
+                      {/* DESCRIPTION INPUT */}
+                      <div className="text-center">
+                        <textarea
+                          type="text"
+                          className="form-control"
+                          placeholder="Please leave your message..."
+                          name="description"
+                          {...register("description", {
+                            required: "Please describe your project needs or a message...",
+                          })}
+                        ></textarea>
+                        <div className="line"></div>
+                      </div>
+                      <span className="error-message">{errors.description && errors.description.message}</span>
+                      <button className="btn-contact contact-btn" type="submit">
+                        contact me
+                      </button>
+                    </div>
+                    {/* 222 */}
+                  </div>
+                </div>
+
+                <div className="col-md-12">
+                  <div className="row"></div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
