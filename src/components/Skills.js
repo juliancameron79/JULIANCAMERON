444785b29c44ce4cc57ee4a1cc8895d45ce87de7
juliancameron79 <juliancameron79@gmail.com>
2021-07-06 00:04:06 +0200

    skills component completed

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,19 +4,21 @@ import { faLaptopCode, faServer, faPencilRuler } from "@fortawesome/free-solid-s
 
 const Skills = () => {
   return (
-    <div id="skills" className="skills">
-      <h1 className="py-5 skills-heading" data-aos="fade-right" data-aos-duration="1000">
+    <div id="skills" className="site-section skills">
+      <h1 className="mb-5 skills-heading" data-aos="fade-right" data-aos-duration="1000">
+        <div className="line-skills me-3" data-aos="fade-right" data-aos-duration="1000"></div>
         Skills
+        <div className="line-skills ms-3" data-aos="fade-right" data-aos-duration="1000"></div>
       </h1>
 
       <div className="container">
         <div className="row">
           <div className="col-md-4 col-sm-6">
             <div className="box frontend-skillset shadow-sm">
-              <div className="icon-wrapper">
-                <FontAwesomeIcon className="skills-icon" icon={faLaptopCode} size="2x" />
-              </div>
-              <div class="py-5 px-4">
+              <div class="py-5">
+                <div className="icon-wrapper mb-5">
+                  <FontAwesomeIcon className="skills-icon" icon={faLaptopCode} size="2x" />
+                </div>
                 <p class="skills-subtitle">Front-end</p>
                 <p class="skills-text">I like to code things from scratch, and enjoy bringing ideas to life in the browser.</p>
                 <p class="skills-color-subtitle">Languages I speak:</p>
@@ -35,10 +37,10 @@ const Skills = () => {
           {/* - */}
           <div className="col-md-4 col-sm-6">
             <div className="box backend-skillset shadow-sm">
-              <div className="icon-wrapper">
-                <FontAwesomeIcon className="skills-icon" icon={faServer} size="2x" />
-              </div>
-              <div class="py-5 px-4">
+              <div class="py-5">
+                <div className="icon-wrapper mb-5">
+                  <FontAwesomeIcon className="skills-icon" icon={faServer} size="2x" />
+                </div>
                 <p class="skills-subtitle">Back-end</p>
                 <p class="skills-text">I like to code things from scratch, and enjoy bringing ideas to life in the browser.</p>
                 <p class="skills-color-subtitle">Languages I speak:</p>
@@ -57,14 +59,14 @@ const Skills = () => {
           {/* - */}
           <div className="col-md-4 col-sm-6">
             <div className="box design-skillset shadow-sm">
-              <div className="icon-wrapper">
-                <FontAwesomeIcon className="skills-icon" icon={faPencilRuler} size="2x" />
-              </div>
-              <div class="py-5 px-4">
+              <div class="py-5">
+                <div className="icon-wrapper mb-5">
+                  <FontAwesomeIcon className="skills-icon" icon={faPencilRuler} size="2x" />
+                </div>
                 <p class="skills-subtitle">Designer</p>
                 <p class="skills-text">I value simple content structure, clean design and thoughtful interactions.</p>
                 <p class="skills-color-subtitle">I enjoy designing:</p>
-                <p class="skills-text">UX, UI, Web, Mobile, Logos</p>
+                <p class="skills-text">UX, UI, Web, Mobile</p>
                 <p class="skills-color-subtitle">Design Tools:</p>
                 <ul class="p-0">
                   <li class="skills-text">Figma</li>
