b1beec6e2513ca6b9545b03054f5550e7527d348
juliancameron79 <juliancameron79@gmail.com>
2021-08-05 11:48:35 +0200

    Made changes to about text and added correct cv

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,73 +17,89 @@ const Header = () => {
                 Julian Cameron
               </h1>
               <div className="display-6 text-white">
-                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="200">
-                  Junior{" "}
-                </span>
                 <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="300">
-                  F
+                  J
                 </span>
                 <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="400">
                   u
                 </span>
                 <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="500">
-                  l
+                  n
                 </span>
                 <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="600">
-                  l
+                  i
                 </span>
                 <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="700">
+                  o
+                </span>
+                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="800">
+                  r
+                </span>
+                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="900">
+                  {" "}
+                  F
+                </span>
+                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="1000">
+                  u
+                </span>
+                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="1100">
+                  l
+                </span>
+                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="1200">
+                  l
+                </span>
+                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="1300">
                   {" "}
                   S
                 </span>
-                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="800">
+                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="1400">
                   t
                 </span>
-                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="900">
+                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="1500">
                   a
                 </span>
-                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="1000">
+                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="1600">
                   c
                 </span>
-                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="1100">
+                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="1700">
                   k
                 </span>
-                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="1200">
+                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="1800">
                   {" "}
                   W
                 </span>
-                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="1300">
-                  e
-                </span>
-                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="1400">
-                  b
-                </span>
-                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="1500">
-                  {" "}
-                  D
-                </span>
-                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="1600">
-                  e
-                </span>
-                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="1700">
-                  v
-                </span>
-                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="1800">
-                  e
-                </span>
                 <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="1900">
-                  l
+                  e
                 </span>
                 <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="2000">
-                  o
+                  b
                 </span>
                 <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="2100">
-                  p
+                  {" "}
+                  D
                 </span>
                 <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="2200">
                   e
                 </span>
                 <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="2300">
+                  v
+                </span>
+                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="2400">
+                  e
+                </span>
+                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="2500">
+                  l
+                </span>
+                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="2600">
+                  o
+                </span>
+                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="2700">
+                  p
+                </span>
+                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="2800">
+                  e
+                </span>
+                <span data-aos="fade" data-aos-easing="ease-in-back" data-aos-delay="2900">
                   r
                 </span>
               </div>
