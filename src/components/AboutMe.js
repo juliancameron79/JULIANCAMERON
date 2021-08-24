00c887b0b77823583cf2fff868487b0059e75b95
juliancameron79 <juliancameron79@gmail.com>
2021-08-24 16:29:55 +0200

    Updated about me text

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -20,7 +20,7 @@ const AboutMe = () => {
           </div>
           <div className="col-lg-6 col-sm-12" data-aos="fade" data-aos-delay="50" data-aos-duration="2000" data-aos-once="true">
             <p className="about-title">Hi, I’m Julian. Nice to meet you.</p>
-            <p className="about-text">I’m a recently qualified Full Stack Web Developer living in Viksjö. I enjoy working with and adapting to technologies as they develop. I'm quietly confident, naturally curious, and perpetually working on improving my chops one design problem at a time.</p>
+            <p className="about-text">I’m a recently qualified Full Stack Software Developer living in Viksjö. I enjoy working with and adapting to technologies as they develop. I'm quietly confident, naturally curious, and perpetually working on improving my chops one design problem at a time.</p>
             <p className="about-text"> Before starting my journey as a developer, I spent nearly 12 years in the corporate environment successfully managing many onsite and offsite projects for large and small firms from across the globe. </p>
           </div>
         </div>
